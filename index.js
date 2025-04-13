c08a5237bc0a1c8705613d73a5affb0d7ba8a3e0
Marcelo Switach <marceloswitach@Mac.fibertel.com.ar>
2025-04-13 20:00:32 -0300

    as

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -10,4 +9,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Servidor escuchando en http://localhost:${PORT}`);
 });
-
